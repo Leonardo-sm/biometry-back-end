83be2abbe995c45b3a6b702d55502327a88a2932
leonardomichelutti-pushstart <leonardo.smichelutti@gmail.com>
2021-11-12 10:08:59 -0300

    🐛 Fix table name in query

--- app/Controllers/Http/MinistersController.ts
+++ app/Controllers/Http/MinistersController.ts
@@ -4,7 +4,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class MinistersController {
   public async index({ params, response }: HttpContextContract) {
     if (params.permission === 'minister') {
-      const data = await Database.query().from('pesticides').select('*')
+      const data = await Database.query().from('pesticedes').select('*')
       return data
     } else {
       response.status(401)
